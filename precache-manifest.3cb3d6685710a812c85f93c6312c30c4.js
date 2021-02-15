ce36e968e65e4cbb110c4e3194b6a5e0c61cf7fd
Rita Curado <rita.curado@thecodeventure.com>
2021-02-15 23:32:55 +0000

    Updates

---
+++ precache-manifest.3cb3d6685710a812c85f93c6312c30c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fd8c2bfb37569cd35c46a9cb4dd9af",
+    "url": "/kata/index.html"
+  },
+  {
+    "revision": "6dba2b04bf86e97f8e38",
+    "url": "/kata/static/css/main.4921b545.chunk.css"
+  },
+  {
+    "revision": "69b9343a6afa6528cb2c",
+    "url": "/kata/static/js/2.69a92978.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/kata/static/js/2.69a92978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dba2b04bf86e97f8e38",
+    "url": "/kata/static/js/main.cf4cbbf9.chunk.js"
+  },
+  {
+    "revision": "be9b9a03c031c2ddb8fc",
+    "url": "/kata/static/js/runtime-main.0e78f7af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kata/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71f5d87687ed95753c0db1cf25d3b473.js"
+  "/kata/precache-manifest.3cb3d6685710a812c85f93c6312c30c4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kata/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
